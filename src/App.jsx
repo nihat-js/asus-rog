5e41b41e7f10628ef372315e77285f0ec7f7a8f8
Nihat <abdullazadenihat@gmail.com>
2022-12-21 02:17:39 +0400

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import './App.scss'
+import Nav from './components/Nav'
 
 export default function App() {
    return (
       <div>
-
+         <Nav> </Nav>
       </div>
    )
 }
